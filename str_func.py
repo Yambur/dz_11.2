f1c8899e03f39ce82b50a39213882f15faa0f8e5
Barg <DIZz92@mail.ru>
2023-09-19 17:44:29 +0300

    add new_def

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def big_boss():
-    word = input()
+    word = input("Введи ченть: ")
     return word.upper()
+
+def all_big_boss():
+    """Короч, заглавные буквы у каждого слова, понял?"""
+    word = input("Введи предложение, реще давай: ")
+    return word.title()
