be5038d03897dc4f809ae453d89b0f717e49074c
Barg <DIZz92@mail.ru>
2023-09-19 21:38:00 +0300

    123321

--- str_func.py
+++ str_func.py
@@ -2,3 +2,8 @@ def big_boss():
     """Выводит ВСЕ буквы с заглавной"""
     word = input()
     return word.upper()
+
+def all_big_boss():
+    """Короч, заглавные буквы у каждого слова, понял?"""
+    word = input("Введи предложение, реще давай: ")
+    return word.title()
